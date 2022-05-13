f8cf4f5a98f67ad520dd19af122da74eccfe26a7
pyonkichimaru <65487947+pyonkichimaru@users.noreply.github.com>
2022-05-13 17:41:17 +0900

    挙動確認の修正2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -41,7 +41,7 @@ class PrototypesController < ApplicationController
   def update
     @prototype=Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to prototype_pat(@prototype)
+      redirect_to prototype_path(@prototype)
     else
       render :edit
     end  
